10b66788ce154e3e6c790bd672218c830918d72d
yixin.panyi <panyi@yixin.im>
2019-07-11 18:42:40 +0800

    切换为正式环境

--- imsdk/src/main/java/com/xinlan/imsdk/Config.java
+++ imsdk/src/main/java/com/xinlan/imsdk/Config.java
@@ -1,5 +1,6 @@
 package com.xinlan.imsdk;
 
 public final class Config {
-    public static final String HTTP_SERVER = "http://10.216.33.169:9999/";
+    //public static final String HTTP_SERVER = "http://10.216.33.169:9999/";
+    public static final String HTTP_SERVER = "http://panyi.xyz:9999/";
 }
